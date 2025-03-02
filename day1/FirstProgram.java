3d1e62e1890ba29dd9ec162174ef24f33400ba99
ShivaMathiv <mathivananshiva@gmail.com>
2025-03-02 14:21:00 +0530

    Add files via upload
    day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package week1.day1;
+public class FirstProgram {
+	
+	public static void main(String[] args ) {
+		System.out.println("welcome to testleaf"); //type syso and do ctrl+space
+		
+	// type main and do ctrl+space
+
+ 
+		
+		
+	}
+}
---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main( String[] args) {
+		int regNo=12345;
+		boolean isACar=true;
+				float speed=0.00f;
+		char logo='5';
+		
+		String name ="Skoda";
+		long phNo=124555;
+		double capactiy = 123.4343345;
+		
+		System.out.println("THE LOGO OF CAR IS:"+logo);
+				System.out.println("regNO is:"+regNo);
+				System.out.println(phNo);
+		System.out.println("regNO is:"+regNo+" "+"name of the car is:"+name);
+				
+	}
+}
